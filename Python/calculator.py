4953856feaed8211f768c3fe0136a543a3c9092a
Jay Patel <jp9573@gmail.com>
2020-10-05 12:48:37 +0530

    Change in operation function map

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,21 +1,21 @@
 def add(x, y):
     # To adds two numbers
-    return x + y
+    return x + y, '+'
 
 
 def subtract(x, y):
     # To subtracts two numbers
-    return x - y
+    return x - y, '-'
 
 
 def multiply(x, y):
     # To multiplies two numbers
-    return x * y
+    return x * y, 'x'
 
 
 def divide(x, y):
     # To divides two numbers
-    return x / y
+    return x / y, '/'
 
 
 print("Select your operation:")
@@ -26,13 +26,6 @@ print("4. Divide")
 print("5. Exit")
 
 operation_mapping = {
-    '1': '+',
-    '2': '-',
-    '3': '*',
-    '4': '/',
-}
-
-operation_function_mapping = {
     '1': add,
     '2': subtract,
     '3': multiply,
@@ -51,7 +44,7 @@ while True:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        res = operation_function_mapping[choice](num1, num2)
-        print("{} {} {} = {}".format(num1, operation_mapping[choice], num2, res))
+        res, sign = operation_mapping[choice](num1, num2)
+        print("{} {} {} = {}".format(num1, sign, num2, res))
     else:
         print("Please enter valid choice")
